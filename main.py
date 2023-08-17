b4674eea30cd657b4e65a98ab9053dd44e039b08
willow <brmagrini@gmail.com>
2023-08-17 19:58:20 -0300

    versão enviando e-mail com data

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def send_emails():
     emails = get_emails()
     emails_str = ','.join(emails)
     gmail_client = wg.WillowGmailClient()
-    label = gmail_client.get_label_id_by_name("me","Desenvolvimento/hospedagem")   
-    message = gmail_client.create_message("me", "brmagrini@gmail.com", emails_str,"Teste de cobrança",message_text )
+    label = gmail_client.get_label_id_by_name("me","Desenvolvimento/hospedagem")
+    date = utils.Utils().get_current_month_year()
+    subject = f"Colaboração para o servidor [{date}]"
+    message = gmail_client.create_message("me", "brmagrini@gmail.com", emails_str,subject,message_text )
     gmail_client.send_message(user_id="me",message=message, labels=[label])
