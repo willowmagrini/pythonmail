a01e3ae76acc529793cb4aeed6a67b5522667da0
willow <brmagrini@gmail.com>
2023-08-17 11:03:56 -0300

    DOCUMENTACAO DAS FUNÇÕES

--- willowspreadsheet.py
+++ willowspreadsheet.py
@@ -1,14 +1,26 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-class GoogleSheetClient:
+class WillowGoogleSheetClient:
+    """
+    A class that provides methods to interact with Google Sheets using the gspread library.
+    
+    Attributes:
+        client: The authorized client for accessing Google Sheets.
+    """
     
     def __init__(self):
+        """
+        Initializes the WillowGoogleSheetClient object and authenticates the client.
+        """
         self.client = self.get_drive_client()
     
     def get_drive_client(self):
         """
-        Autentica usando as credenciais e retorna um cliente autorizado.
+        Authenticates the client using the credentials and returns an authorized client.
+        
+        Returns:
+            The authorized client for accessing Google Sheets.
         """
         scope_spreadsheet = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
         try:
@@ -16,24 +28,33 @@ class GoogleSheetClient:
             client = gspread.authorize(creds)
             return client
         except Exception as e:
-            print("Erro ao criar cliente: ", e)
+            print("Error creating client: ", e)
             return None
     
     def get_sheet(self, name):
         """
-        Abre a planilha pelo nome e retorna a primeira aba da planilha.
+        Opens the specified sheet by name and returns the first worksheet of the sheet.
+        
+        Args:
+            name: The name of the sheet to open.
+        
+        Returns:
+            The first worksheet of the specified sheet.
         """
         try:
             sheet = self.client.open(name)
             sheet_instance = sheet.get_worksheet(0)
             return sheet_instance
         except Exception as e:
-            print("Erro ao acessar a planilha: ", e)
+            print("Error accessing sheet: ", e)
             return None
     
     def get_clients(self):
         """
-        Retorna todos os registros da planilha 'Clientes hospedagem'.
+        Returns all the records from the 'Clientes hospedagem' sheet.
+        
+        Returns:
+            A list of dictionaries representing the records from the sheet.
         """
         sheet_instance = self.get_sheet('Clientes hospedagem')
         if sheet_instance:
@@ -43,19 +64,37 @@ class GoogleSheetClient:
     
     def filter_active_clients(self, clients):
         """
-        Filtra os clientes com Status igual a 1.
+        Filters the clients with Status equal to 1.
+        
+        Args:
+            clients: A list of clients to filter.
+        
+        Returns:
+            A new list of clients with Status equal to 1.
         """
         return [item for item in clients if item['Status'] == 1]
     
     def get_client_email(self, client):
         """
-        Retorna o campo de e-mail de um cliente.
+        Returns the email field of a client.
+        
+        Args:
+            client: A dictionary representing a client.
+        
+        Returns:
+            The email field of the client. If the email field is not present, returns an empty string.
         """
         return client.get('email', '')
 
     def get_clients_emails(self, clients):
         """
-        Retorna uma lista com os e-mails de uma lista de clientes.
+        Returns a list of emails from a list of clients.
+        
+        Args:
+            clients: A list of clients.
+        
+        Returns:
+            A list of emails extracted from the clients' dictionaries.
         """
         return [client['email'] for client in clients if 'email' in client]
 
