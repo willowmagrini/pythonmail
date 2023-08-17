dedef0360bd0893d5dab127b4d82c5079d854d54
willow <brmagrini@gmail.com>
2023-08-17 10:51:57 -0300

    refatoração para classe no spreadsheet

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import willowgmail as wg
 import willowspreadsheet as ws
 
 def get_emails():
-    drive_client = ws.get_drive_client()
-    clients = ws.get_clients()
-    active_clients = ws.filter_active_clients(clients)
-    emails = ws.get_clients_emails(active_clients)
-    return emails
+    sheet_client = ws.GoogleSheetClient()
+    clients = sheet_client.get_clients()
+    active_clients = sheet_client.filter_active_clients(clients)
+    clients_emails = sheet_client.get_clients_emails(active_clients)
+    return clients_emails
 
 def send_emails():
     message_text = "Chegou aquele momento mensal de colaborar para nosso servidor não sair do ar!\nPIX: 34510394861\nBruno Antunes Magrini\nQuando fizer a colaboração manda o comprovante na resposta desse email por favor."
@@ -15,3 +15,9 @@ def send_emails():
     service = wg.get_gmail_service()
     message = wg.create_message("me", emails_str,"Teste de cobrança",message_text  )
     wg.send_message(service=service,user_id="me",message=message)
+
+
+    clients_data = sheet_client.get_clients()
+    active_clients = sheet_client.filter_active_clients(clients_data)
+    clients_emails = sheet_client.get_clients_emails(active_clients)
+    print(clients_emails)
--- willowspreadsheet.py
+++ willowspreadsheet.py
@@ -1,32 +1,69 @@
 import gspread
-import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 
-def get_drive_client():
-    scope_spreadsheet = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials-spreadsheet.json', scope_spreadsheet)
-    client = gspread.authorize(creds)
-    return client
+class GoogleSheetClient:
+    
+    def __init__(self):
+        self.client = self.get_drive_client()
+    
+    def get_drive_client(self):
+        """
+        Autentica usando as credenciais e retorna um cliente autorizado.
+        """
+        scope_spreadsheet = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+        try:
+            creds = ServiceAccountCredentials.from_json_keyfile_name('credentials-spreadsheet.json', scope_spreadsheet)
+            client = gspread.authorize(creds)
+            return client
+        except Exception as e:
+            print("Erro ao criar cliente: ", e)
+            return None
+    
+    def get_sheet(self, name):
+        """
+        Abre a planilha pelo nome e retorna a primeira aba da planilha.
+        """
+        try:
+            sheet = self.client.open(name)
+            sheet_instance = sheet.get_worksheet(0)
+            return sheet_instance
+        except Exception as e:
+            print("Erro ao acessar a planilha: ", e)
+            return None
+    
+    def get_clients(self):
+        """
+        Retorna todos os registros da planilha 'Clientes hospedagem'.
+        """
+        sheet_instance = self.get_sheet('Clientes hospedagem')
+        if sheet_instance:
+            return sheet_instance.get_all_records()
+        else:
+            return []
+    
+    def filter_active_clients(self, clients):
+        """
+        Filtra os clientes com Status igual a 1.
+        """
+        return [item for item in clients if item['Status'] == 1]
+    
+    def get_client_email(self, client):
+        """
+        Retorna o campo de e-mail de um cliente.
+        """
+        return client.get('email', '')
 
-def get_sheet(client, name):
-    sheet = client.open(name)
-    sheet_instance = sheet.get_worksheet(0)
-    return sheet_instance
+    def get_clients_emails(self, clients):
+        """
+        Retorna uma lista com os e-mails de uma lista de clientes.
+        """
+        return [client['email'] for client in clients if 'email' in client]
 
-def get_clients(client):
-    sheet_instance = get_sheet(client, 'Clientes hospedagem')
-    records_data = sheet_instance.get_all_records()
-    return records_data
 
-def filter_active_clients(clients):
-    filtered_data = [item for item in clients if item['Status'] == 1]
-    return filtered_data
-
-def get_client_email(client):
-    return client['email']
-
-def get_clients_emails(clients):
-    emails = []
-    for client in clients:
-        emails.append(client['email'])
-    return emails
+# Exemplo de uso da classe:
+if __name__ == '__main__':
+    sheet_client = GoogleSheetClient()
+    clients_data = sheet_client.get_clients()
+    active_clients = sheet_client.filter_active_clients(clients_data)
+    clients_emails = sheet_client.get_clients_emails(active_clients)
+    print(clients_emails)
